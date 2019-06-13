cfd3bdf6f617947e86ace26b29ed82f7fcf122a8
LinH <742571766@qq.com>
2019-06-13 16:01:13 +0800

    删除多余文件

--- app/src/main/java/com/example/haolin/userdefinedcamera/MainActivity.java
+++ app/src/main/java/com/example/haolin/userdefinedcamera/MainActivity.java
@@ -1,12 +1,39 @@
 package com.example.haolin.userdefinedcamera;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.haolin.camera.CameraSurfaceView;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private CameraSurfaceView mCameraSurfaceView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 全屏显示
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        mCameraSurfaceView = findViewById(R.id.cameraSurfaceView);
+        mCameraSurfaceView.setAutoFocus();
+        ImageButton takePicBtn = findViewById(R.id.takePic);
+        takePicBtn.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.takePic:
+                mCameraSurfaceView.takePicture();
+                break;
+            default:
+                break;
+        }
     }
 }
